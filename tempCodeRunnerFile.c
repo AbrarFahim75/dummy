b9699636dc796508002bc74fde34c0a302cce29d
AbrarFahim75 <abrarfahim1948@gmail.com>
2024-11-07 22:05:30 +0100

    changing loop name

--- tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-    // // Convert to uppercase using strupr() (note: non-standard in some compilers)
-    // for (int i = 0; greeting[i]; i++) {
-    //     greeting[i] = toupper(greeting[i]);
-    // }
-    // printf("Uppercase String: %s\n", greeting);
